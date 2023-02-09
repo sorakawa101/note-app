5ef6e8cc23f8c66400fa0cc7087df073ae283a75
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-02-09 09:08:09 +0900

    add: Sidebarコンポーネントの雛形を作成

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,21 @@ import "./Sidebar.css"
 
 function Sidebar() {
     return (
-    <div>
-        Sidebar
+    <div className='app-sidebar'>
+        <div className='app-sidebar-header'>
+            <h1>Note</h1>
+            <button>Add</button>
+        </div>
+        <div className="app-sidebar-notes">
+            <div className="app-sidebar-note">
+                <div className="sidebar-note-title">
+                    <strong>Title</strong>
+                    <button>Delete</button>
+                </div>
+                <p>Note Contents</p>
+                <small>Fixed Date: 02/09</small>
+            </div>
+        </div>
     </div>
     )
 }
